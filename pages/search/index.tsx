d27d0b6eaf37c956e4826de5371a536465797e84
chamegashi <msymsy0693wrb@gmail.com>
2022-04-21 21:37:16 +0900

    add localstrage

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,16 +1,23 @@
 import { Box, Button, Center, Input } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Link from "next/link";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 import Template from "../../src/components/template/Template";
+import { musicDataState } from "../../src/states/localStrage";
 
 const Search: NextPage = () => {
   const [searchWord, setSearchWord] = useState<string>("");
+  const { addSearchLog, getSearchLogs, strageData } = musicDataState();
 
   const changeKeyword = (e: FormEvent<HTMLInputElement>) => {
     setSearchWord(e.currentTarget.value);
   };
+
+  useEffect(() => {
+    console.log(addSearchLog("testData"));
+    console.log(strageData);
+  }, []);
 
   return (
     <Template title="検索" backUrl="/">
@@ -38,6 +45,7 @@ const Search: NextPage = () => {
             <Button colorScheme={"teal"}>検索！</Button>
           </Link>
         </Center>
+        <Center>{strageData}</Center>
       </Box>
     </Template>
   );
--- src/states/localStrage.ts
+++ src/states/localStrage.ts
@@ -1,8 +1,25 @@
 import { useState } from "react";
+import store from "store2";
 
 export const musicDataState = () => {
-  const [strageData] = useState("");
+  const [strageData, setStrageData] = useState("");
   const [loading] = useState(false);
 
-  return { strageData, loading };
+  const setStore = (key: string, data: string) => {
+    store.set(key, data);
+  };
+
+  const getStore = (key: string) => {
+    return store.remove(key);
+  };
+
+  const addSearchLog = (addLog: string) => {
+    const logs = store.get("searchLog");
+  };
+
+  const getSearchLogs = () => {
+    setStrageData(getStore("searchLog"));
+  };
+
+  return { strageData, loading, addSearchLog, getSearchLogs };
 };
